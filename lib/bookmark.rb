c7a1d0b443660da20595b7a6e2c09025050446d0
Graham <grahamalconer@icloud.com>
2020-04-08 14:52:38 +0100

    connect test for table passed

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,13 +1,47 @@
+require 'pg'
+
 class Bookmark
 
   def self.show_list
+    storage = []
+    con = PG.connect :dbname => 'bookmarks', :user => 'graham'
+    rows = con.exec "SELECT * FROM bookmark_list;"
+    #rows = con.exec "SELECT * bo"
     @bookmark = [Bookmark.new, Bookmark.new]
+    rows.each do |element|
+      storage << element['url']
+    end
+    return storage
   end
 
 end
 
+Bookmark.show_list
+
 
 =begin
-  def show_list
-  end
+begin
+
+    con = PG.connect :dbname => 'testdb', :user => 'janbodnar'
+
+    rs = con.exec "SELECT * FROM Cars LIMIT 5"
+
+    rs.each do |row|
+      puts "%s %s %s" % [ row['id'], row['name'], row['price'] ]
+    end
+
+rescue PG::Error => e
+
+    puts e.message
+
+ensure
+
+    rs.clear if rs
+    con.close if con
+
+end
+
+- Use PG to connect to the PostgreSQL bookmark_manager database.
+- Retrieve all bookmark records from the bookmarks table.
+- Extract the URLs from the database response.
 =end
--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -1,10 +1,33 @@
 require 'bookmark'
 
+
 describe Bookmark do
   it 'displays the users bookmarks in a list' do
     expect(Bookmark.show_list).to be_instance_of(Array)
   end
-  it "displays a list of bookmarks" do
-    expect(Bookmark.show_list[0].class).to eq(Bookmark)
+  it 'displays a list of bookmarks' do
+    expect(Bookmark.show_list[0].class).to eq(String)
   end
+  it 'returns the urls from the table' do
+    con = PG.connect :dbname => 'bookmarks', :user => 'graham'
+    expect(Bookmark.show_list).to include('http://www.makersacademy.com')
+    .and include('http://www.google.com')
+    .and include('http://askjeeves.com')
+    .and include('http://www.google.com/')
+  end
+
 end
+
+=begin
+id |             url
+----+------------------------------
+ 2 | http://www.makersacademy.com
+ 3 | http://www.google.com
+ 4 | http://askjeeves.com
+(3 rows)
+
+
+
+con = PG.connect :dbname => 'bookmark', :user => 'graham'
+
+=end
